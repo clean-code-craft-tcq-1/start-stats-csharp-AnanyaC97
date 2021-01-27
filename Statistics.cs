ed63c1621a9aaf37670362e4a81ab5b73e185be9
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-01-27 14:03:36 +0530

    Add files via upload

---
+++ Statistics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Statistics
+{
+    public class StatsComputer
+    {
+
+        public double average = 0.0;
+        public double max = 0;
+        public double min = 0;
+        public const double NaN = Double.NaN;
+        public List<double> CalculateStatistics(List<double> numbers)
+        {
+            //Implement statistics here
+            average = numbers.AsQueryable().Average();
+            max = numbers.Max();
+            min = numbers.Min();
+
+            List<double> results = new List<double> { average, max, min };
+            return results;
+        }
+    }
+}
